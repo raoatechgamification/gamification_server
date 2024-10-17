35d4b5adbc599e929a77845e27d53c47db9aec5a
Timpel <75267154+oluwaseun-odueso@users.noreply.github.com>
2024-10-17 12:55:56 +0100

    chore: user billing endpoint

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -3,7 +3,13 @@ import { authenticate, authorize } from "../middlewares/auth.middleware";
 import { UserController } from "../controllers/user.controller";
 import { editUserProfileValidator } from "../validators/user.auth.validator";
 
-const { editProfile } = new UserController();
+const { 
+  editProfile, 
+  billHistory, 
+  dueBills,  
+  viewBill, 
+  addCard
+} = new UserController();
 
 const router = Router();
 
@@ -14,5 +20,33 @@ router.put(
   ...editUserProfileValidator,
   editProfile
 );
+
+router.get(
+  "/bill-history", 
+  authenticate, 
+  authorize("user"),
+  billHistory
+);
+
+router.get(
+  "/due-bills",
+  authenticate, 
+  authorize("user"),
+  dueBills
+)
+
+router.get(
+  "/view-bill/:paymentId", 
+  authenticate,
+  authorize("user"),
+  viewBill
+)
+
+router.post(
+  "/add-card",
+  authenticate,
+  authorize("user"),
+  addCard
+)
 
 export default router;
