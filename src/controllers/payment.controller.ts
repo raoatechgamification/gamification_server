c98b85aa07ff2a31e820ffa37bc74312d8ab639e
Timpel <75267154+oluwaseun-odueso@users.noreply.github.com>
2024-10-29 17:00:01 +0100

    fix: modified webhook controller

--- src/controllers/payment.controller.ts
+++ src/controllers/payment.controller.ts
@@ -1,5 +1,4 @@
 import { Request, Response, NextFunction } from "express";
-import crypto from "crypto";
 import PaymentService from "../services/payment.service";
 import User from "../models/user.model";
 import CourseAccess from "../models/courseAssess.model";
@@ -7,10 +6,9 @@ import Bill, { BillDocument } from "../models/bill.model";
 import AssignedBill from "../models/assignedBill.model";
 import Payment from "../models/payment.model";
 import { ResponseHandler } from "../middlewares/responseHandler.middleware";
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 import axios from "axios";
 dotenv.config();
-
 
 class PaymentController {
   async processPayment(req: Request, res: Response, next: NextFunction) {
@@ -51,7 +49,7 @@ class PaymentController {
         userId,
         billId: bill._id,
         assignedBillId,
-        status: "pending",
+        status: "outstanding",
         reference,
       });
 
@@ -69,7 +67,8 @@ class PaymentController {
     const { transactionId } = req.params;
 
     try {
-      const verificationData = await PaymentService.verifyPayment(transactionId)
+      const verificationData =
+        await PaymentService.verifyPayment(transactionId);
 
       // const url = `https://api.flutterwave.com/v3/transactions/${transactionId}/verify`;
 
@@ -83,23 +82,26 @@ class PaymentController {
       // const verificationData = response.data;
 
       // Check if the transaction was successful
-      if (verificationData.status === 'success' && verificationData.data.status === 'successful') {
+      if (
+        verificationData.status === "success" &&
+        verificationData.data.status === "successful"
+      ) {
         return res.status(200).json({
-          message: 'Payment verified successfully',
+          message: "Payment verified successfully",
           data: verificationData.data,
         });
       } else {
         // Transaction is not successful
         return res.status(400).json({
-          message: 'Payment verification failed',
+          message: "Payment verification failed",
           data: verificationData.data,
         });
       }
     } catch (error: any) {
-      console.error('Error verifying payment:', error);
-      
+      console.error("Error verifying payment:", error);
+
       return res.status(500).json({
-        message: 'Server error while verifying payment',
+        message: "Server error while verifying payment",
         error: error.response?.data || error.message,
       });
     }
@@ -107,45 +109,51 @@ class PaymentController {
 
   async paymentWebhook(req: Request, res: Response, next: NextFunction) {
     try {
-      const flutterwaveVerifHash = req.headers['verif-hash'];
+      const flutterwaveVerifHash = req.headers["verif-hash"];
       const secretHash = process.env.FLUTTERWAVE_SECRET_HASH;
-  
+
       if (flutterwaveVerifHash !== secretHash) {
         console.log("Hash mismatch - Unauthorized request");
         return res.status(403).json({ message: "Invalid signature" });
       }
-  
+
       const data = req.body;
       console.log("Webhook received with data (request body)", data);
-  
+
       if (data.status === "successful") {
         console.log("Transaction reference:", data.txRef);
-  
+
         // Retrieve the payment using the correct reference key
         const payment = await Payment.findOne({ reference: data.txRef });
-  
+
         if (payment) {
           const userId = payment.userId;
           const billId = payment.billId;
           console.log("User and Bill IDs:", userId, billId);
-  
-          await CourseAccess.create({
-            userId,
-            billId,
-            hasAccess: true
-          });
-  
+
+          // await CourseAccess.create({
+          //   userId,
+          //   billId,
+          //   hasAccess: true
+          // });
+
           await Payment.updateOne(
             { _id: payment._id }, // or { userId: userId } if unique
             { status: "successful", data },
             { new: true }
           );
-  
+
+          await AssignedBill.updateOne(
+            { _id: billId },
+            { status: "paid" },
+            { paymentId: payment._id }
+          );
+
           console.log("Payment successful and completed");
         }
       } else if (data.status === "failed") {
         const payment = await Payment.findOne({ reference: data.txRef });
-  
+
         if (payment) {
           await Payment.updateOne(
             { _id: payment._id },
@@ -155,24 +163,22 @@ class PaymentController {
         }
         console.log("Payment failed:", data);
       }
-  
+
       res.status(200).json({ status: "success" });
     } catch (error: any) {
       console.error("Error in payment webhook:", error);
       res.status(500).json({
         message: "Server error",
-        error: error.message
+        error: error.message,
       });
     }
   }
-  
 
-  
   // async paymentWebhook(req: Request, res: Response, next: NextFunction) {
   //   try {
   //     const flutterwaveVerifHash = req.headers['verif-hash'];
   //     const secretHash = process.env.FLUTTERWAVE_SECRET_HASH;
-      
+
   //     if (flutterwaveVerifHash !== secretHash) {
   //       console.log("Hash mismatch - Unauthorized request");
 
@@ -198,7 +204,7 @@ class PaymentController {
   //         console.log("This is user and bill ids: ", userId, billId)
 
   //         await CourseAccess.create({
-  //           userId, 
+  //           userId,
   //           billId,
   //           hasAccess: true
   //         })
@@ -209,12 +215,12 @@ class PaymentController {
   //           { new: true }
   //         )
 
-  //         // Send success notification to the user 
+  //         // Send success notification to the user
   //         console.log("Payment successful and completed");
   //       }
   //     } else if (data.status === "failed") {
   //       const payment = await Payment.findOne({ reference: data.tx_ref})
-        
+
   //       if (payment) {
   //         const userId = payment.userId
 
@@ -229,13 +235,12 @@ class PaymentController {
 
   //     res.status(200).json({ status: "success" });
   //   } catch (error: any) {
-  //     res.status(500).json({ 
+  //     res.status(500).json({
   //       message: "Server error",
-  //       error: error.message 
+  //       error: error.message
   //     });
   //   }
   // }
-
 
   // async verifyPayment(req: Request, res: Response, next: NextFunction) {
   //   try {
