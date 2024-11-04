983108fb3107f12a75a4b721b60581ad2e80d0e6
Timpel <75267154+oluwaseun-odueso@users.noreply.github.com>
2024-11-04 16:46:10 +0100

    feat: update user password

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -3,6 +3,7 @@ import { ResponseHandler } from "../middlewares/responseHandler.middleware";
 import User from "../models/user.model";
 import Payment from "../models/payment.model";
 import AssignedBill from "../models/bill.model";
+import { comparePassword, hashPassword } from "../utils/hash"
 
 export class UserController {
   async editProfile(req: Request, res: Response, next: NextFunction) {
@@ -119,4 +120,37 @@ export class UserController {
     }
   }
 
+  async updatePassword (req: Request, res: Response) {
+    try {
+      const { currentPassword, newPassword } = req.body;
+      const userId = req.user.id;
+
+      const user = await User.findById(userId)
+      if ( !user ) return ResponseHandler.failure(res, "User not found", 404);
+
+      const hashedPassword = user.password;
+
+      const currentPasswordIsValid = await comparePassword(
+        currentPassword,
+        hashedPassword
+      );
+
+      if (!currentPasswordIsValid) {
+        return ResponseHandler.failure(
+          res,
+          "You have entered an incorrect password",
+          400
+        );
+      }
+
+      const newHashedPassword = await hashPassword(newPassword)
+      user.password = newHashedPassword;
+      user.save()
+    } catch (error: any) {
+      res.status(500).json({
+        message: "Server error",
+        error: error.message,
+      });
+    }
+  }
 }
