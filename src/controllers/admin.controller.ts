ded0684c2810bd657105806eeae84f42ed6ce5db
Timpel <75267154+oluwaseun-odueso@users.noreply.github.com>
2024-11-07 08:31:15 +0100

    feat: admin controller methods done

--- src/controllers/admin.controller.ts
+++ src/controllers/admin.controller.ts
@@ -1,57 +1,70 @@
-import { Request, Response} from "express";
+import { Request, Response } from "express";
 import { ResponseHandler } from "../middlewares/responseHandler.middleware";
-import User, { IUser, UserDocument } from "../models/user.model";
-import Organization, { IOrganization } from "models/organization.model";
+import Payment from "../models/payment.model";
+import AssignedBill from "../models/bill.model";
+import User, { UserDocument } from "../models/user.model";
 
 class AdminController {
-  async viewAllUsers (req: Request, res: Response) {
+  async viewAllUsers(req: Request, res: Response) {
     try {
-      const organizationId = req.admin._id
+      const organizationId = req.admin._id;
 
-      const users: UserDocument[] = await User.find({ organizationId })
+      const users = await User.find({ organizationId });
 
-      if (!users) {
+      if (!users || users.length === 0) {
         return ResponseHandler.failure(
-          res, 
-          "You have no user under your organization, start by creating users",
+          res,
+          "You have no users under your organization, start by creating users",
           400
-        )
+        );
       }
 
-      return ResponseHandler.success(res, users, "Users fetched successfully")
+      const usersWithDetails = await Promise.all(
+        users.map(async (user) => {
+          const paymentHistory = await Payment.find({ userId: user._id });
+          const assignedBills = await AssignedBill.find({
+            organizationId,
+            "assignee.individuals": user._id,
+          });
+          return { ...user.toObject(), paymentHistory, assignedBills };
+        })
+      );
+
+      return ResponseHandler.success(
+        res,
+        usersWithDetails,
+        "Users fetched successfully"
+      );
     } catch (error: any) {
       return ResponseHandler.failure(
         res,
         `Server error: ${error.message}`,
         500
-      )
+      );
     }
   }
 
-  async editUserProfile (req: Request, res: Response) {
+  async editUserProfile(req: Request, res: Response) {
     try {
-      const organizationId = req.admin._id
-
-      const userId = req.params.userId
-
+      const organizationId = req.admin._id;
+      const userId = req.params.userId;
       const { firstName, lastName, batch, role } = req.body;
+
+      const user = await User.findOne({ _id: userId, organizationId });
+
+      if (!user) {
+        return ResponseHandler.failure(
+          res,
+          "User not found in your organization",
+          404
+        );
+      }
 
       const updatedUser = await User.findByIdAndUpdate(
         userId,
-        {
-          $set: {
-            firstName,
-            lastName,
-            batch,
-            role
-          },
-        },
-        { new: true, runValidators: true } 
-      )
-
-      if (!updatedUser) {
-        return ResponseHandler.failure(res, "User not found", 404);
-      }
+        { $set: { firstName, lastName, batch, role } },
+        { new: true, runValidators: true }
+      );
 
       return ResponseHandler.success(
         res,
@@ -63,25 +76,49 @@ class AdminController {
         res,
         `Server error: ${error.message}`,
         500
-      )
+      );
     }
   }
 
-  async viewAUserProfile (req: Request, res: Response ) {
+  async viewAUserProfile(req: Request, res: Response) {
     try {
-      const organizationId = req.admin._id
-
+      const organizationId = req.admin._id;
       const userId = req.params.userId;
 
-      // Assigned Bills History
+      const user = await User.findOne({ _id: userId, organizationId });
 
-      // Payment History
+      if (!user) {
+        return ResponseHandler.failure(
+          res,
+          "User not found in your organization",
+          404
+        );
+      }
+
+      const paymentHistory = await Payment.find({ userId: user._id });
+
+      const assignedBills = await AssignedBill.find({
+        organizationId,
+        "assignee.individuals": user._id,
+      });
+
+      const userWithDetails = {
+        ...user.toObject(),
+        paymentHistory,
+        assignedBills,
+      };
+
+      return ResponseHandler.success(
+        res,
+        userWithDetails,
+        "User profile fetched successfully"
+      );
     } catch (error: any) {
       return ResponseHandler.failure(
         res,
         `Server error: ${error.message}`,
         500
-      )
+      );
     }
   }
 }
