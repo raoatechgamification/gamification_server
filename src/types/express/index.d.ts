28a1dc5ed8e8674a751a9760b2c2fa17eaf7f99e
Timpel <75267154+oluwaseun-odueso@users.noreply.github.com>
2024-12-30 07:45:33 +0100

    chore: chores

--- src/types/express/index.d.ts
+++ src/types/express/index.d.ts
@@ -2,6 +2,8 @@
 import { User } from "../../models/user.model";
 import { Organization } from "../../models/organization.model";
 import { SuperAdmin } from "../../models/superadmin.model";
+import { UploadedFile } from 'express-fileupload';
+
 
 declare global {
   namespace Express {
@@ -9,6 +11,9 @@ declare global {
       user?: User;
       admin?: Organization | undefined;
       superAdmin?: SuperAdmin | undefined;
+      files?: {
+        [key: string]: UploadedFile | UploadedFile[];
+      };
     }
   }
 }
