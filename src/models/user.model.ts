f976561bf7a3c474bacf98718ed69fdcad9e98de
Timpel <75267154+oluwaseun-odueso@users.noreply.github.com>
2025-01-09 08:51:01 +0100

    chore: user model modified

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -12,10 +12,10 @@ const OngoingProgramSchema = new Schema<IOngoingProgram>({
 });
 
 export interface IAssignedProgram {
-  _id: Schema.Types.ObjectId; 
+  _id: Schema.Types.ObjectId;
   courseId: Schema.Types.ObjectId;
   dueDate: Date;
-  status: "paid" | "pending" | "unpaid" | "free"; 
+  status: "paid" | "pending" | "unpaid" | "free";
   amount: number;
 }
 
@@ -28,6 +28,7 @@ export interface IUser extends Document {
   phone?: string;
   userId?: string;
   groups?: mongoose.Schema.Types.ObjectId[];
+  subLearnerGroups?: mongoose.Schema.Types.ObjectId[];
   organizationId?: mongoose.Schema.Types.ObjectId;
   role: string;
   password: string;
@@ -48,59 +49,64 @@ export interface IUser extends Document {
   officeLGA?: string;
   officeState?: string;
   employerName?: string;
-  assignedPrograms?: IAssignedProgram[],
+  assignedPrograms?: IAssignedProgram[];
   ongoingPrograms?: { course: ICourse }[];
   completedPrograms?: { course: ICourse }[];
-  unattemptedPrograms?: { course: ICourse, status: "paid" | "pending" | "unpaid" | "free" }[];
-  // unattemptedPrograms?: ICourse[];
-  certificates?: { 
-    courseId: mongoose.Types.ObjectId, 
+  unattemptedPrograms?: {
+    course: ICourse;
+    status: "paid" | "pending" | "unpaid" | "free";
+  }[];
+  certificates?: {
+    courseId: mongoose.Types.ObjectId;
     courseName: string;
-    certificateId: mongoose.Types.ObjectId
-  }[]
-  createdAt: Date; 
-  updatedAt: Date; 
+    certificateId: mongoose.Types.ObjectId;
+  }[];
+  createdAt: Date;
+  updatedAt: Date;
   lessonCompletionStatus?: {
     [courseId: string]: {
-      [lessonId: string]: number;  
+      [lessonId: string]: number;
     };
   };
 }
 
 const AssignedProgramSchema = new Schema<IAssignedProgram>(
   {
-    _id: { type: Schema.Types.ObjectId, auto: true }, 
+    _id: { type: Schema.Types.ObjectId, auto: true },
     courseId: { type: Schema.Types.ObjectId, ref: "Course", required: true },
     dueDate: { type: Date, required: true },
-    status: { 
-      type: String, 
-      enum: ["paid", "pending", "unpaid", "free"], 
-      required: true 
+    status: {
+      type: String,
+      enum: ["paid", "pending", "unpaid", "free"],
+      required: true,
     },
     amount: { type: Number, required: true },
   },
-  { _id: false } 
+  { _id: false }
 );
 
 const UserSchema: Schema<IUser> = new Schema(
   {
     username: { type: String, unique: false },
-    firstName: { type: String, required: true, },
-    lastName: { type: String, required: true, },
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
     otherName: { type: String, default: null },
-    email: { type: String, required: true, unique: true, },
+    email: { type: String, required: true, unique: true },
     phone: { type: String, sparse: true },
     userId: { type: String, default: null },
-    groups: [{ type: mongoose.Schema.Types.ObjectId, sparse: true }],
-    organizationId: { type: mongoose.Schema.Types.ObjectId, default: null, },
-    role: { type: String, default: "user", required: true, },
-    password: { type: String, required: true, },
+    groups: [
+      { type: mongoose.Schema.Types.ObjectId, sparse: true, ref: "Group" },
+    ],
+    subLearnerGroups: [{ type: mongoose.Schema.Types.ObjectId }],
+    organizationId: { type: mongoose.Schema.Types.ObjectId, default: null },
+    role: { type: String, default: "user", required: true },
+    password: { type: String, required: true },
     batch: { type: String },
     userType: { type: String, default: "learner" },
-    yearsOfExperience: { type: Number, default: null, },
-    highestEducationLevel: { type: String, default: null, },
-    gender: { type: String, default: null, },
-    dateOfBirth: { type: String, default: null, },
+    yearsOfExperience: { type: Number, default: null },
+    highestEducationLevel: { type: String, default: null },
+    gender: { type: String, default: null },
+    dateOfBirth: { type: String, default: null },
     image: { type: String, default: null },
     country: { type: String, default: null },
     address: { type: String, default: null },
@@ -113,14 +119,9 @@ const UserSchema: Schema<IUser> = new Schema(
     officeState: { type: String, default: null },
     employerName: { type: String, default: null },
     assignedPrograms: {
-      type: [AssignedProgramSchema], 
+      type: [AssignedProgramSchema],
       default: [],
     },
-    // ongoingPrograms: { type: [OngoingProgramSchema], default: [] },
-    // completedPrograms: { 
-    //   type: [{ type: Object }],
-    //   default: [],
-    //  },
     ongoingPrograms: [
       {
         course: {
@@ -129,7 +130,10 @@ const UserSchema: Schema<IUser> = new Schema(
           objective: { type: String },
           certificate: { type: mongoose.Types.ObjectId, ref: "Certificate" },
           tutorId: { type: mongoose.Types.ObjectId, ref: "Tutor" },
-          organizationId: { type: mongoose.Types.ObjectId, ref: "Organization" },
+          organizationId: {
+            type: mongoose.Types.ObjectId,
+            ref: "Organization",
+          },
           lessonFormat: { type: String },
           lessons: { type: Array, default: [] },
           assignedLearnersIds: { type: [mongoose.Types.ObjectId], default: [] },
@@ -141,7 +145,11 @@ const UserSchema: Schema<IUser> = new Schema(
           learnerIds: { type: [mongoose.Types.ObjectId], default: [] },
           __v: { type: Number },
         },
-        status: { type: String, enum: ["paid", "pending", "unpaid", "free"], required: true },
+        status: {
+          type: String,
+          enum: ["paid", "pending", "unpaid", "free"],
+          required: true,
+        },
       },
     ],
     completedPrograms: [
@@ -152,7 +160,10 @@ const UserSchema: Schema<IUser> = new Schema(
           objective: { type: String },
           certificate: { type: mongoose.Types.ObjectId, ref: "Certificate" },
           tutorId: { type: mongoose.Types.ObjectId, ref: "Tutor" },
-          organizationId: { type: mongoose.Types.ObjectId, ref: "Organization" },
+          organizationId: {
+            type: mongoose.Types.ObjectId,
+            ref: "Organization",
+          },
           lessonFormat: { type: String },
           lessons: { type: Array, default: [] },
           assignedLearnersIds: { type: [mongoose.Types.ObjectId], default: [] },
@@ -164,7 +175,11 @@ const UserSchema: Schema<IUser> = new Schema(
           learnerIds: { type: [mongoose.Types.ObjectId], default: [] },
           __v: { type: Number },
         },
-        status: { type: String, enum: ["paid", "pending", "unpaid", "free"], required: true },
+        status: {
+          type: String,
+          enum: ["paid", "pending", "unpaid", "free"],
+          required: true,
+        },
       },
     ],
     unattemptedPrograms: [
@@ -175,7 +190,10 @@ const UserSchema: Schema<IUser> = new Schema(
           objective: { type: String },
           certificate: { type: mongoose.Types.ObjectId, ref: "Certificate" },
           tutorId: { type: mongoose.Types.ObjectId, ref: "Tutor" },
-          organizationId: { type: mongoose.Types.ObjectId, ref: "Organization" },
+          organizationId: {
+            type: mongoose.Types.ObjectId,
+            ref: "Organization",
+          },
           lessonFormat: { type: String },
           lessons: { type: Array, default: [] },
           assignedLearnersIds: { type: [mongoose.Types.ObjectId], default: [] },
@@ -187,23 +205,22 @@ const UserSchema: Schema<IUser> = new Schema(
           learnerIds: { type: [mongoose.Types.ObjectId], default: [] },
           __v: { type: Number },
         },
-        status: { type: String, enum: ["paid", "pending", "unpaid", "free"], required: true },
+        status: {
+          type: String,
+          enum: ["paid", "pending", "unpaid", "free"],
+          required: true,
+        },
       },
     ],
-    // unattemptedPrograms: [
-    //   {
-    //     course: { type: {}, ref: "Course" }, 
-    //     status: { type: String }
-    //   }
-    // ],
     certificates: [
       {
         courseId: { type: mongoose.Types.ObjectId, ref: "Course" },
-        courseName: { type: String},
-        certificateId: { type: mongoose.Types.ObjectId, ref: "Certifcate"}
-      }
+        courseName: { type: String },
+        certificateId: { type: mongoose.Types.ObjectId, ref: "Certifcate" },
+      },
     ],
-  }, {
+  },
+  {
     timestamps: true,
   }
 );
