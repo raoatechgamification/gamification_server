57c7dc88f0f0883d3df5ea2049e5fb4e271a6083
Timpel <75267154+oluwaseun-odueso@users.noreply.github.com>
2025-01-16 07:15:18 +0100

    feat: sub admin controller

---
+++ src/controllers/auth/auth.subadmin.controller.ts
@@ -0,0 +1,182 @@
+import dotenv from "dotenv";
+import { Request, Response } from "express";
+import User from "../../models/user.model";
+import SubAdmin from "../../models/subadmin.model"
+import Role from "../../models/subadmin.model";
+import SuperAdmin, { ISuperAdmin } from "../../models/superadmin.model";
+import Organization, { IOrganization } from "../../models/organization.model";
+import Permission from "../../models/permission.model";
+import { ResponseHandler } from "../../middlewares/responseHandler.middleware"; 
+import { uploadToCloudinary } from "../../utils/cloudinaryUpload"
+import { comparePassword, hashPassword } from "../../utils/hash";
+import { generateToken } from "../../utils/jwt";
+
+
+
+export class SubAdminController {
+  async createSubAdminAccount(req: Request, res: Response) {
+    try {
+      let {
+        firstName,
+        lastName,
+        otherName,
+        email,
+        phone,
+        userId,
+        gender,
+        dateOfBirth,
+        country,
+        address,
+        city,
+        LGA,
+        state,
+        officeAddress,
+        officeCity,
+        officeLGA,
+        officeState,
+        employerName,
+        role,
+        batch,
+        password,
+        sendEmail,
+        contactPersonPlaceOfEmployment,
+        nameOfContactPerson,
+        contactEmail,
+        contactPersonPhoneNumber,
+      } = req.body;
+
+      const image = req.file;
+      const organizationId = req.admin._id;
+
+      let fileUploadResult: any = null;
+
+      if (image) {
+        fileUploadResult = await uploadToCloudinary(
+          image.buffer,
+          image.mimetype,
+          "userDisplayPictures"
+        );
+      }
+
+      if (!password) {
+        password = `${firstName}${lastName}123#`;
+      }
+
+      const organization = await Organization.findById(organizationId);
+      if (!organization) {
+        return ResponseHandler.failure(res, "Organization not found", 400);
+      }
+
+      const existingAccount =
+        (await Organization.findOne({ email })) ||
+        (await User.findOne({ email })) ||
+        (await SuperAdmin.findOne({ email }));
+
+      if (existingAccount) {
+        return ResponseHandler.failure(res, "Email already registered", 400);
+      }
+
+      const hashedPassword = await hashPassword(password);
+
+      const newUser = await SubAdmin.create({
+        firstName,
+        lastName,
+        otherName,
+        email,
+        phone,
+        userId,
+        gender,
+        dateOfBirth,
+        image: fileUploadResult ? fileUploadResult.secure_url : null,
+        country,
+        address,
+        city,
+        LGA,
+        state,
+        officeAddress,
+        officeCity,
+        officeLGA,
+        employerName,
+        officeState,
+        password: hashedPassword,
+        organizationId,
+        batch,
+        contactPersonPlaceOfEmployment,
+        contactEmail,
+        nameOfContactPerson,
+        contactPersonPhoneNumber,
+        userType: role,
+      });
+
+      const response = await SubAdmin.findById(newUser._id).select(
+        "-password -role"
+      );
+
+      return res.status(201).json({
+        message: "Sub-admin account created successfully",
+        data: response,
+        loginUrl: `${process.env.FRONTENT_BASEURL}/auth/login`,
+      });
+    } catch (error: any) {
+      return res.status(500).json({
+        success: false,
+        message: "An error occurred while creating sub-admin account",
+        error: error.message,
+      });
+    }
+  }
+
+  async loginSubAdmin(req: Request, res: Response) {
+    try {
+      const { email, password } = req.body;
+
+      const registeredUser = await SubAdmin.findOne({ email });
+
+      if (!registeredUser) {
+        return ResponseHandler.failure(res, "Sub-admin does not exist", 400);
+      }
+
+      const checkPassword = await comparePassword(
+        password,
+        registeredUser.password
+      );
+
+      if (!checkPassword) {
+        return ResponseHandler.failure(
+          res,
+          "You have entered an incorrect password",
+          400
+        );
+      }
+
+      const payload = {
+        id: registeredUser._id,
+        email: registeredUser.email,
+        username: registeredUser.username,
+        phone: registeredUser.phone,
+        organizationId: registeredUser.organizationId,
+        firstName: registeredUser.firstName,
+        lastName: registeredUser.lastName,
+        role: registeredUser.role,
+      };
+
+      const token = await generateToken(payload);
+
+      const response = await SubAdmin.findById(registeredUser._id).select(
+        "-password -role"
+      );
+
+      return ResponseHandler.loginResponse(
+        res,
+        token,
+        response,
+        "Login Successful"
+      );
+    } catch (error: any) {
+      res.status(500).json({
+        message: "Server error",
+        error: error.message,
+      });
+    }
+  }
+}
